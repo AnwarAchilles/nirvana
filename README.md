aef8fad089999d95d6b7f2f5b512bc8089f1cf8a
AnwarAchilles <hudorianwar07@gmail.com>
2024-01-08 19:20:22 +0700

    update

--- README.md
+++ README.md
@@ -28,3 +28,6 @@ Install all composer required
 - Basic Frontend (JS)
 - Layouting Engine
 - Offline Support
+
+#
+[![portfolio](https://ik.imagekit.io/anwarachilles/devneet-powered.svg?updatedAt=1704715329026)]('#')
--- application/config/config.php
+++ application/config/config.php
@@ -23,9 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-// $config['base_url'] = 'https://d410d5b3048fa094c57b1ecd1c68cb86.serveo.net/';
-$config['base_url'] = 'https://devneet.test/';
-// $config['base_url'] = 'http://192.168.1.53/Devneet';
+$config['base_url'] = 'https://__project.test/nirvana/';
 
 /*
 |--------------------------------------------------------------------------
